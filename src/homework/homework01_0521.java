705a42673be7bd9a3ecce3c3603d7db1a12878e3
yonycats <dusgml1953@gmail.com>
2024-05-21 21:56:58 +0900

    Collection 정렬 (Comparable, Comparator) 학습 및 과제

---
+++ src/homework/homework01_0521.java
@@ -0,0 +1,217 @@
+package homework;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class homework01_0521 {
+	public static void main(String[] args) {
+		
+		/*
+		학번, 이름, 국어점수, 영어점수, 수학점수, 총점, 등수를 멤버로 갖는
+		  Student클래스를 만든다.
+		  생성자는 학번, 이름, 국어, 영어, 수학 점수만 매개변수로 받아서 처리한다.
+		  // 학번은 문자로 정의할 것 (String)
+		  
+		  이 Student객체들은 List에 저장하여 관리한다.
+		  List에 저장된 데이터들을 학번의 오름차순으로 정렬하여 출력하는 부분과
+		  총점의 역순으로 정렬하는 부분을 프로그램 하시오.
+		  (총점이 같으면 학번의 내림차순으로 정렬되도록 한다.) => 총점이 같은 값 추가해놓기
+		  (학번 정렬기준은 Student클래스 자체에서 제공하도록 하고, => Comparable 사용
+		   총점 정렬기준은 외부클래스에서 제공하도록 한다.) => Comparator 사용
+		*/
+		
+		List<Student> strData = new ArrayList<Student>();
+		
+		strData.add(new Student("F001", "양요섭", 50, 70, 50)); // 총점 170
+		strData.add(new Student("C001", "윤두준", 80, 50, 70)); // 총점 200
+		strData.add(new Student("A001", "이기광", 70, 60, 30)); // 총점 160
+		strData.add(new Student("E001", "손동운", 40, 90, 70)); // 총점 200
+		strData.add(new Student("D001", "이도현", 60, 80, 60)); // 총점 200
+		strData.add(new Student("B001", "유재석", 50, 70, 70)); // 총점 190
+		
+		
+		
+		// 학번의 오름차순으로 정렬하여 출력
+		// (학번 정렬기준은 Student클래스 자체에서 제공)
+		Collections.sort(strData);
+		
+		System.out.println("학번순으로 출력");
+		for (Student student : strData) {
+			System.out.println(student);
+		}
+		System.out.println("=================================================");
+		
+		
+		
+		// 총점의 역순으로 정렬 (총점이 같으면 학번의 내림차순으로 정렬)
+		// (총점 정렬기준은 외부클래스에서 제공)
+		Collections.sort(strData, new SortRankDesc());
+		
+		System.out.println("총점의 역순으로 출력(총점이 같으면 학번의 내림차순으로)");
+		for (Student student : strData) {
+			System.out.println(student);
+		}
+		System.out.println("=================================================");
+		
+		
+	
+		// 등수 매기기 (같은 점수는 같은 등수)
+		int rankNum = 1;
+        for (int i = 0; i < strData.size(); i++) {
+            if (i!=0 && strData.get(i).getSumScore() < strData.get(i - 1).getSumScore()) {
+            	rankNum = i + 1;
+            }
+            strData.get(i).setRank(rankNum);
+        }
+        
+		System.out.println("등수 매기기 (같은 점수는 같은 등수)");
+		for (Student student : strData) {
+			System.out.println(student);
+		}
+		System.out.println("=================================================");
+		
+	}
+	
+}
+
+class Student implements Comparable<Student> {
+
+	String strNum;
+	String name;
+	int korScore;
+	int engScore;
+	int mathScore;
+	int sumScore;
+	int rank;
+	
+
+	public Student(String strNum, String name, int korScore, int engScore, int mathScore) {
+		super();
+		this.strNum = strNum;
+		this.name = name;
+		this.korScore = korScore;
+		this.engScore = engScore;
+		this.mathScore = mathScore;
+		sumScore = korScore+engScore+mathScore;
+	}
+	
+	
+
+	public String getStrNum() {
+		return strNum;
+	}
+
+
+
+	public void setStrNum(String strNum) {
+		this.strNum = strNum;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public int getKorScore() {
+		return korScore;
+	}
+
+
+
+	public void setKorScore(int korScore) {
+		this.korScore = korScore;
+	}
+
+
+
+	public int getEngScore() {
+		return engScore;
+	}
+
+
+
+	public void setEngScore(int engScore) {
+		this.engScore = engScore;
+	}
+
+
+
+	public int getMathScore() {
+		return mathScore;
+	}
+
+
+
+	public void setMathScore(int mathScore) {
+		this.mathScore = mathScore;
+	}
+
+
+
+	public int getSumScore() {
+		return sumScore;
+	}
+
+
+
+	public void setSumScore(int sumScore) {
+		this.sumScore = sumScore;
+	}
+
+
+
+	public int getRank() {
+		return rank;
+	}
+
+
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Student [strNum=" + strNum + ", name=" + name + ", korScore=" + korScore + ", engScore=" + engScore
+				+ ", mathScore=" + mathScore + ", sumScore=" + sumScore + ", rank=" + rank + "]";
+	}
+
+	
+	// 학번의 오름차순으로 정렬하여 출력
+	// (학번 정렬기준은 Student클래스 자체에서 제공)
+	@Override
+	public int compareTo(Student o) {
+		return getStrNum().compareTo(o.getStrNum());
+	}
+
+}
+
+
+// 총점의 역순으로 정렬 (총점이 같으면 학번의 내림차순으로 정렬)
+// (총점 정렬기준은 외부클래스에서 제공)
+class SortRankDesc implements Comparator<Student> {
+
+	@Override
+	public int compare(Student o1, Student o2) {
+		
+		if(o1.getSumScore() > o2.getSumScore()) {
+			return -1;
+		} else if(o1.getSumScore() == o2.getSumScore()) {
+			return o1.getStrNum().compareTo(o2.getStrNum())*-1;
+		} else return 1;
+	}
+	
+}
--- src/kr/or/ddit/basic/T01ArrayListTest.java
+++ src/kr/or/ddit/basic/T01ArrayListTest.java
@@ -7,7 +7,8 @@ public class T01ArrayListTest {
 	public static void main(String[] args) {
 		
 		// Default Capacity = 10
-		List<Object> list1 = new ArrayList<Object>();
+		List list1 = new ArrayList();
+		// 인터페이스 기반 코딩을 하면 재사용성이 늘어나고, 유지보수에 용이함
 		
 		// add() 메서드를 사용하여 데이터를 추가한다.
 		list1.add("aaa");
@@ -39,7 +40,47 @@ public class T01ArrayListTest {
 		
 		list1.remove("bbb");
 		System.out.println("bbb 데이터 삭제 후 : " + list1);
+		
+		list1.remove(new Integer(111));
+		System.out.println("111 데이터 삭제 후 : " + list1);
 		System.out.println("-----------------------------------");
+		
+		// 제너릭을 이용하여 선언할 수 있다.
+		List<String> list2 = new ArrayList<String>();
+		list2.add("AAA");
+		list2.add("BBB");
+		list2.add("CCC");
+		list2.add("DDD");
+		list2.add("EEE");
+		
+		for (String str : list2) {
+			System.out.println(str);
+		}
+		System.out.println("-----------------------------------");
+		
+		// contains(비교객체) => 리스트에 '비교객체'가 존재하면 true, 없으면 false 리턴함.
+		System.out.println(list2.contains("DDD"));
+		System.out.println(list2.contains("ZZZ"));
+		
+		// indexOf(비교객체) => 리스트에서 '비교객체'를 찾아 존재하는 인덱스값을 반환한다.
+		// 					   존재하지 않으면 -1을 반환한다.
+		System.out.println("DDD의 index값 : " +list2.indexOf("DDD"));
+		System.out.println("DDD의 index값 : " +list2.indexOf("ZZZ"));
+		System.out.println("-----------------------------------");
+		
+		for (int i=0; i <list2.size(); i++) {
+			list2.remove(i);
+		}
+		System.out.println("list2의 갯수 : "+ list2.size());
+		
+//		for (int i=0; i <list2.size(); i++) {
+//			System.out.println(list2.get(i));
+//		}
+		// 리무브를 했는데 전부 다 지워지지 않고 부분적으로 남는 이유
+		// => 리스트가 지워짐과 동시에 자동정렬되기 때문 + 시스템에 부하가 일어남
+		// => 반대로 맨 뒤부터 지우면 됨, 또는 clear() 사용
+		list2.clear();
+		System.out.println("list2의 갯수 : "+ list2.size());
 		
 	}
 }
